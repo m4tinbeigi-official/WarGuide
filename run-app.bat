e0b0d39db19bacc825d4e8b587d6d2469f5d1646
Rick Sanchez <matinbeigi99@gmail.com>
2025-06-16 14:02:27 +0330

    Update run-app.bat

--- run-app.bat
+++ run-app.bat
@@ -1,1 +1,12 @@
+@echo off
+echo Building the project...
 npm run build
+
+echo Deleting old docs folder if exists...
+rmdir /S /Q docs
+
+echo Renaming 'dist' folder to 'docs'...
+rename dist docs
+
+echo Done!
+pause
